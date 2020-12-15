ca5c7e71d9a71429955c08208b41ea5575d3f50d
timwilliam <timothywilliam.cs06g@g2.nctu.edu.tw>
2020-12-15 13:52:45 +0800

    Completed p389

--- cpe-p389-basically-speaking.c
+++ cpe-p389-basically-speaking.c
@@ -1,17 +1,21 @@
+// Program	: CPE - p389 Basically Speaking
+// Author	: Timothy William
+// Date		: 12/15/2020
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
 
 int main(void){
-    char buffer[100], input[100], output[10], *token, temp;
-    int base, target, in_len, i, base_10, digit;
+    char buffer[100], input[100], output[7], *token, temp;
+    int base, target, in_len, i, base_10, digit, quotient;
 
     while(fgets(buffer, 100, stdin) != NULL){
         if(buffer[0] == '\n') break;
         buffer[strcspn(buffer, "\n")] = 0; // Remove newline character
 
-        base_10 = 0;
+        base_10 = 0; memset(input, 0, 100); memset(output, 0, 7);
 
         //TODO 1. Parse the buffer
         token = strtok(buffer, " ");
@@ -20,7 +24,6 @@ int main(void){
         base = atoi(token); // Get the base
         token = strtok(NULL, " ");
         target = atoi(token); // Get the target base
-        //printf("Buffer: %s %d %d\n", input, base, target);
 
         //TODO 2. Convert input number to decimal
         in_len = strlen(input); // Get input char length
@@ -34,9 +37,29 @@ int main(void){
         }
 
         //TODO 3. Convert decimal to target base
-        if(target == 10) printf("%d\n", base_10);
-        else{
-            continue; 
-        }
-    }
+		i = 0;
+		while(base_10 > 0){
+			quotient = base_10 % target;
+			base_10 = base_10 / target;
+			
+			// Convert to char representation
+			if(quotient >= 10) output[i] = quotient - 10 + 'A';
+			else output[i] = quotient + '0';
+
+			i++;
+			if(i == 7) break; // Exceed 7-digit display limit
+		}
+
+		// Print out the conversion result
+		if(i == 7){
+			printf("ERROR\n");	
+			continue;
+		}
+
+		while(i >= 0){
+			printf("%c", output[i]);
+			i--;
+		}
+		printf("\n");
+	}
 }
