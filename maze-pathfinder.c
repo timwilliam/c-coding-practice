bd02e2e2d3e41e2ed6a01fbd768743f92fa340c7
timwilliam <timothywilliam.cs06g@g2.nctu.edu.tw>
2020-12-21 13:13:06 +0800

    Some bugs in the linked list stack version, paused for now

--- maze-pathfinder.c
+++ maze-pathfinder.c
@@ -19,7 +19,35 @@ int END[2] = {0, 2}; // Ends at top right
 int X_MAX = 2;
 int Y_MAX = 2;
 
-void get_path(int current_pos[], int prev_pos[], int avail_path[]){
+struct Path{
+	int xy[2];
+	struct Path *link;
+};
+
+struct Path *path_history = NULL; // To store all of the path taken
+
+void push(int path[], struct Path **stack){
+	struct Path *temp = malloc(sizeof(struct Path));
+
+	temp->xy[0] = path[0];
+	temp->xy[1] = path[1];
+
+	printf("ptr now %p, temp %p\n", stack, temp);
+	temp->link = *stack;
+	*stack = temp;
+
+	printf("ptr then %p\n", stack);
+}
+
+void pop(struct Path **stack){
+	struct Path *temp;
+	if(*stack == NULL) return;
+	temp = *stack;
+	(*stack) = temp->link;
+	free(temp);
+}
+
+int get_path(int current_pos[], int prev_pos[], struct Path *avail_path){
 	int i, j, k;
 	int path_candidate[4][2]; // Up=0, Right=1, Down=2, Left=3
 	int valid_path[4] = {0}; // Set to 1 if a direction is valid, else 0
@@ -37,8 +65,6 @@ void get_path(int current_pos[], int prev_pos[], int avail_path[]){
 	// Left
 	path_candidate[3][0] = current_pos[0];
 	path_candidate[3][1] = current_pos[1] - 1;
-
-	//printf("Up:%d,%d Down:%d,%d Right:%d,%d Left:%d,%d\n", path_candidate[0][0], path_candidate[0][1], path_candidate[1][0], path_candidate[1][1], path_candidate[2][0], path_candidate[2][1], path_candidate[3][0], path_candidate[3][1]);
 
 	//TODO Check if any of the 4 directions is out of bounds (maze)
 	for(i = 0; i < 4; i++){
@@ -58,15 +84,24 @@ void get_path(int current_pos[], int prev_pos[], int avail_path[]){
 			valid_path[i] = 0;
 	}
 	
+	//TODO Store all the avail_path in a stack
+	int n_avail_path = 0;
 	for(i = 0; i < 4; i++){
-		//if(valid_path[i] == 1) printf("Valid %d,%d\n", path_candidate[i][0], path_candidate[i][1]);
-		if(valid_path[i] == 1) break;
+		// Create a stack to store the avail_path
+		if(valid_path[i] == 1){
+			push(path_candidate[i], &avail_path);
+			printf("cek %d\n", avail_path->xy[0]);
+			n_avail_path++;
+		}
 	}
 
-	//TODO Return the avail_path
-	// for now, assume that only 1 available path
-	avail_path[0] = path_candidate[i][0];
-	avail_path[1] = path_candidate[i][1];
+	struct Path *tes = avail_path;
+	while(tes != NULL){
+		printf("xy coor %d,%d\n", tes->xy[0], tes->xy[1]);
+		tes = tes->link;
+	}
+
+	return n_avail_path;
 }
 
 void find_path(int current_pos[], int prev_pos[]){
@@ -77,14 +112,21 @@ void find_path(int current_pos[], int prev_pos[]){
 	}
 
 	//TODO Get available path from current_pos
-	int avail_path[2];
 	
 	//TODO Check if there is available path to go
-	get_path(current_pos, prev_pos, avail_path);
-	
+	struct Path *avail_path = NULL; // To store all of the available path
+	int n_avail_path;
+	n_avail_path = get_path(current_pos, prev_pos, avail_path);
+	printf("n %d\n", n_avail_path);
+
 	//TODO Traverse all available path
-	printf("From %d,%d. Goint to %d,%d\n", current_pos[0], current_pos[1], avail_path[0], avail_path[1]);
-	find_path(avail_path, current_pos);
+	while(n_avail_path > 0){
+		printf("a %d\n", avail_path->xy[0]);
+		printf("* find_path go to %d,%d from %d,%d\n", avail_path->xy[0], avail_path->xy[1], current_pos[0], current_pos[1]);
+		find_path(avail_path->xy, current_pos);
+		pop(&avail_path);
+		n_avail_path--;
+	}
 }
 
 int main(void){
