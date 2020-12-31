facd92927abd2982413ab6425eab637a2708dd18
timwilliam <timothywilliam.cs06g@g2.nctu.edu.tw>
2020-12-31 14:44:01 +0800

    Completed p11960 implementation using the key answer

--- cpe-p11960-divisor-game-key.c
+++ cpe-p11960-divisor-game-key.c
@@ -6,16 +6,13 @@ int Table[1000001], Ans[1000001];
 void Create_Table(){
 	int Max = 0, ans;
 
-	for(int i=1; i <= 6; i++){
-		printf("i=%d\n", i);
-		for(int j=i; j <= 6; j=j+i){	
+	for(int i=1; i <= 1000000; i++){
+		for(int j=i; j <= 1000000; j=j+i){	
 			Table[j] += 1;
-			printf("	j=%d, Table[%d]=%d\n", j, j, Table[j]);
 		}
 	}
 
-	printf("ANS:\n");
-	for(int i=1; i <= 6; i++){
+	for(int i=1; i <= 1000000; i++){
 		if(Max <= Table[i]){
 			Max = Table[i];
 			ans = i;
@@ -24,18 +21,15 @@ void Create_Table(){
 		else{
 			Ans[i] = ans;
 		}
-		printf("- i=%d, Ans[%d]=%d\n", i, i, Ans[i]);
 	}
 }
 
 int main(void){
 	int n, m;
 	Create_Table();
-	//scanf("%d", &n);
-	n = 1;
-	while(n--){	
-		//scanf("%d", &m);
-		m = 6;
+	scanf("%d", &n);
+	while(n--){
+		scanf("%d", &m);	
 		printf("%d\n", Ans[m]);
 	}
 }
