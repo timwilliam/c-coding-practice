14053e8b3830806e4fcf1b603f6f503f5724a5d3
timwilliam <timothywilliam.cs06g@g2.nctu.edu.tw>
2021-03-22 13:14:48 +0800

    Paused working on CPE - p846 for now

--- cpe-p846-steps.c
+++ cpe-p846-steps.c
@@ -1,41 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 int main (void){
-	int T, x, y, range, i, sum, n_step, increment;
+	int T, x, y, target_range;
+	int i, j, n_step, range_covered, inc;
 
-	range = 4;
+	// Create a table containing range_covered, as well as n_step;
+	// - Based on the idea of drawing a pyramid, e.g.:
+	// 						range_covered	n_step
+	// 			1				1			1
+	// 		1	2	1			4			3
+	//	1	2	3	2	1		9			5
 
+	// Max range to be covered is 2^31
+	
 	n_step = 1;
-	for(i = 2; i <= range; i++){
-		n_step = n_step + 2; 
+	while(range_covered < 10000){
+		inc = 1;
+		range_covered = 0;
+
+		for(i = 1; i <= n_step; i++){
+			range_covered += inc;
+			//printf("- step %d, inc %d, range %d\n", n_step, inc, range_covered);
+
+			if(i >= ((n_step + 1) / 2))
+				inc -= 1;
+			else
+				inc += 1;
+		}
+
+		
+		n_step += 2;
 	}
-	//printf("n_step %d\n", n_step);
-	
-	sum = 0; increment = 1;
-	for(i = 0; i < n_step; i++){
-		sum += increment;
-	
-		//printf("increment %d\n", increment);	
-		if(i >= ((n_step + 1) / 2) - 1)
-			increment -= 1;
-		else
-			increment ++;
 
-		//printf("i = %d, sum = %d\n", i, sum);
-	}
-
-	printf("sum %d\n", sum);
-
-	
+	printf("n_step %d, range %d\n", n_step, range_covered);
 	exit(0);
+
 	scanf("%d", &T);
 	while(T--){
 		scanf("%d %d", &x, &y);
 
-		range = y - x;
-		//range = 5;
-
+		target_range = y - x;
+		
 	}
 }
