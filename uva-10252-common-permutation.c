07304fb3e1d38ec90d271136705c9a1693dc7e98
Timothy William <timothywilliam.cs06g@g2.nctu.edu.tw>
2021-04-10 21:34:55 +0800

    Completed uva/10252

--- uva-10252-common-permutation.c
+++ uva-10252-common-permutation.c
@@ -1,49 +1,56 @@
+// Program  : UVA 10252 - Common Permutation
+// Author   : timwilliam
+// Compiled : 04/10/2021  
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(void){
     char a[1000], b[1000], str_len, i;
-    int prmtn[26];
+    int count_a[26], count_b[26], n;
 
     while(1){
         memset(a, 0, sizeof(a));
         memset(b, 0, sizeof(b));
-        memset(prmtn, 0, sizeof(prmtn));
+        memset(count_a, 0, sizeof(count_a));
+        memset(count_b, 0, sizeof(count_b));
 
+        // Break if EOF is inputted
         if(fgets(a, 1000, stdin) == NULL)
             break;
 
+        // Remove trailing "\n" character
         a[strcspn(a, "\n")] = 0;
 
         fgets(b, 1000, stdin);
         b[strcspn(b, "\n")] = 0;
 
+        // Count the number of char occurences in a
         str_len = strlen(a);
         for(i = 0; i < str_len; i++){
-            prmtn[a[i] - 'a'] = 1;
+            count_a[a[i] - 'a'] += 1;
         }
 
-        /*
-        for(i = 0; i < 26; i++){
-            printf("%d, ", prmtn[i]);
-        }
-        printf("\n");*/
-
+        // Count the number of char occurences in b
         str_len = strlen(b);
         for(i = 0; i < str_len; i++){
-            if(prmtn[b[i] - 'a'] == 2) continue;
-            prmtn[b[i] - 'a'] += 1;
+            count_b[b[i] - 'a'] += 1;
         }
 
-        /*
+        // Print out the permutation of a and b if any
         for(i = 0; i < 26; i++){
-            printf("%d, ", prmtn[i]);
-        }
-        printf("\n");*/
-        
-        for(i = 0; i < 26; i++){
-            if(prmtn[i] > 1)
+            // This is to handle case where a character occur more than once
+            //  in both a and b  
+            if(count_a[i] >= 1 && count_b[i] >= 1){
+                n = count_a[i];
+                if(count_b[i] < n)
+                    n = count_b[i];
+            }
+            else
+                n = 0;
+
+            while(n--)
                 printf("%c", i + 'a');
         }
         printf("\n");
