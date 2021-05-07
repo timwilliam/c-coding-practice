98911c20172b9cf769c8fbd4b95ce4aa385615eb
timwilliam <timothywilliam.cs06g@g2.nctu.edu.tw>
2021-05-07 14:36:24 +0800

    Minotr update for  uva/11417

--- uva-10642-can-you-solve-it.c
+++ uva-10642-can-you-solve-it.c
@@ -1,50 +1,57 @@
+// Program	: UVA - 10642
+// Author	: timwilliam
+// Compiled	: 05/07/2021
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(void){
-	int n, x, y, x_path[100], y_path[100], i, j, k, n_step, counter, limit, x_des, y_des;
+	int n, x, y, x_path[1000000], y_path[1000000], i, j, k, n_step, counter, limit, x_des, y_des;
 
 	memset(x_path, 0, sizeof(x_path));
 	memset(y_path, 0, sizeof(y_path));
 
-	// generate x_path
+	// generate y_path
 	limit = 1;
 	counter = limit;
-	for(i = 0; i < 15; i++){
+	for(i = 0; i < 1000000; i++){
+		if(i == 0){
+			y_path[i] = 0;
+			continue;
+		}
+
+		if(counter >= 0 && counter <= limit){
+			y_path[i] = counter;
+			counter--;
+		}
+		else{
+			limit++;
+			y_path[i] = limit;
+			counter = limit - 1;
+		}
+
+	}	
+
+	// NOTE: Algorithm works, but might not cover the entire possible input space, need a better way
+	//printf("limit %d, counter %d\n", limit, counter);
+
+	// generate x_path
+	limit = 1;
+	counter = 0;
+	for(i = 0; i < 1000000; i++){
 		if(i == 0){
 			x_path[i] = 0;
 			continue;
 		}
 
-		if(counter >= 0 && counter <= limit){
-			x_path[i] = counter;
-			counter--;
-		}
-		else{
-			limit++;
-			x_path[i] = limit;
-			counter = limit - 1;
-		}
-
-	}	
-	
-	// generate y_path
-	limit = 1;
-	counter = 0;
-	for(i = 0; i < 15; i++){
-		if(i == 0){
-			y_path[i] = 0;
-			continue;
-		}
-
 		if(counter <= limit){
-			y_path[i] = counter;
+			x_path[i] = counter;
 			counter++;
 		}
 		else{
 			limit++;
-			y_path[i] = 0;
+			x_path[i] = 0;
 			counter = 1;
 		}
 
@@ -56,18 +63,16 @@ int main(void){
 		n_step = 0;
 
 		// get the starting point
-		for(i = 0; i < 15; i++){
+		for(i = 0; i < 1000000; i++){
 			if(x == x_path[i] && y == y_path[i]){
-				printf("i start %d\n", i);
 				n_step = i;
 				break;
 			}
 		}
 
 		// calculate steps taken
-		for(i = i; i < 15; i++){
+		for(i = i; i < 1000000; i++){
 			if(x_des == x_path[i] && y_des == y_path[i]){
-				printf("i end %d\n", i);
 				n_step = i - n_step;
 				break;
 			}
