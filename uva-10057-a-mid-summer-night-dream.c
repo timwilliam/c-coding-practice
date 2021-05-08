a4154cb98cb48f841d19ae160215b7112859d3d3
timwilliam <timothywilliam.cs06g@g2.nctu.edu.tw>
2021-05-08 18:19:52 +0800

    uva/10242 v2, one more thing to do

--- uva-10057-a-mid-summer-night-dream.c
+++ uva-10057-a-mid-summer-night-dream.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 int main(void){
-    int x[1000000], n, i, j, A_min, n_A, n_possible_A, sum, min_sum, A;
+    int x[1000000], n, i, j, A, A_min, n_of_A_min, n_of_psbl_A, sum, min_sum;
 
     while(scanf("%d", &n) != EOF){
         memset(x, 0, sizeof(x));
@@ -12,55 +12,45 @@ int main(void){
             scanf("%d", &x[i]);
         }
 
-        // minimum possible value for A
-        //min_sum = 65536;
-        A_min = 0;
+        // 1. Get minimum possible value for A
+        
+        
+        // Repeat for all possible value of A derived from x
+        for(i = n - 1; i >= 0; i--){
+            sum = 0;
+            for(j = n - 1; j >= 0; j--){
+                // x[j] here is X1, X2, ..., Xn
+                // x[i] here is A
+                sum += abs(x[j] - x[i]);
+            }
+
+            //printf("[DBG] A=%d, sum=%d\n", x[i], sum);
+
+            if(i == n - 1 || sum <= min_sum){
+                min_sum = sum;
+                A_min = x[i]; 
+            }
+            
+            //printf("[DBG] A_min = %d\n", A_min);
+        }
+
+        // how many input satisfy property of A
+        n_of_A_min = 0;
         for(i = 0; i < n; i++){
             sum = 0;
             for(j = 0; j < n; j++){
                 sum += abs(x[j] - x[i]);
             }
 
-            //printf("    %d Current Sum %d, min_sum=%d\n", i, sum, min_sum);
-            if(sum <= min_sum || i == 0){
-                if(sum == min_sum)
-                    continue;            
-    
-                min_sum = sum;
-                A_min = x[i];
-                //printf("    UPDATE: min_sum=%d, A_min=%d\n", min_sum, A_min);
-            }
-        }
-  
-        //printf("min_sum=%d, A_min=%d\n", min_sum, A_min); 
-        // how many input satisfy property of A
-        n_A = 0;
-        for(i = 0; i < n; i++){
-            if(abs(x[i] - A_min) == 0){
-                n_A++;
+            if(sum == min_sum){
+                n_of_A_min++;
             }
         }
 
         // how many possible values for A
-        n_possible_A = 0;
-        A = 1;
-        while(1){
-            sum = 0;
-            for(i = 0; i < n; i++){
-                sum += abs(x[i] - A);
-            }
+        n_of_psbl_A = 0;    
 
-            if(sum <= min_sum){
-                n_possible_A++;
-            }
-            
-            A++;
-            if(A == 65536){
-                break;
-            }
-        }
-    
         // Print the result
-        printf("%d %d %d\n", A_min, n_A, n_possible_A);
+        printf("%d %d %d\n", A_min, n_of_A_min, n_of_psbl_A);
     }
 }
