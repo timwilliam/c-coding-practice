2e902fd05d32c9475c49d3fbd2831c82bbe6f8af
timwilliam <timothywilliam.cs06g@g2.nctu.edu.tw>
2021-05-29 16:20:57 +0800

    Completed uva/10050

--- uva-online-judge/uva-10050-hartals.c
+++ uva-online-judge/uva-10050-hartals.c
@@ -1,9 +1,31 @@
+// Problem  : UVA 10050 - Hartals
+// Author   : timwilliam
+// Compiled : 05/29/2021
+
 #include <stdio.h>
 #include <stdlib.h>
 
+int cmpfunc(const void *a, const void *b){
+    return(*(int*) a - *(int*) b);
+}
+
+int is_in_list(int *list, int n, int hartal_count){
+    int i, status = 0;
+
+    for(i = 1; i <= hartal_count; i++){
+        if(list[i] == n){
+            //printf("--> Found %d in the list!\n", list[i]);
+            status = 1;
+            break;
+        }
+    }
+
+    return status;
+}
+
 int main(void){
 	int T, N, P, *hartal_param, **hartals, n_hartals, *list_of_hartals;
-	int i, j;
+	int i, j, hartal_count, friday;
 
 	scanf("%d", &T);
 	while(T--){
@@ -25,19 +47,56 @@ int main(void){
 				hartals[i][j] = j  * hartal_param[i];
 		}
 
-		/*
-		for(i = 0; i < P; i++){
-			n_hartals = hartals[i][0];
-			printf("n_hartals %d\n", n_hartals);
-			for(j = 1; j <= n_hartals; j++)
-				printf("%d ", hartals[i][j]);
+		// go through hartals of each party and store them
+        n_hartals = 0;
+        for(i = 0; i < P; i++)
+            n_hartals += hartals[i][0];
 
-			printf("\n");
-		}*/
-
-		// store the hartals
+        list_of_hartals = malloc(n_hartals * sizeof(int));
+        hartal_count = 0;
+        
 		for(i = 0; i < P; i++){
-			for(j = 0; j )
+            friday = 6;
+            // store hartals in a list
+			for(j = 1; j <= hartals[i][0]; j++){
+                //printf("now at party %d, hartals = %d\n", i+1, hartals[i][j]);
+
+                // hartals already exist in list
+                if(is_in_list(list_of_hartals, hartals[i][j], hartal_count)){
+                    //printf("--> Already in list, skip!\n");
+                    continue;
+                }
+                else{
+                    // check if it is Friday or Saturday
+                    while(hartals[i][j] > friday)
+                        friday += 7;
+
+                    if(hartals[i][j] % friday == 0 || hartals[i][j] % 7 == 0){
+                        //printf("--> It is Friday(%d)/Saturday, skip (%d)!\n", friday, hartals[i][j]);
+                        continue;
+                    }
+                    else{
+                        list_of_hartals[hartal_count] = hartals[i][j];
+                        hartal_count++;             
+                    }
+                }
+            }
 		}
+        
+        qsort(list_of_hartals, hartal_count, sizeof(int), cmpfunc);
+        /*
+        for(i = 0; i < hartal_count; i++)
+            printf("%d ", list_of_hartals[i]);
+        printf("\n");
+        */
+
+        printf("%d\n", hartal_count);
+
+        free(list_of_hartals);
+        free(hartal_param);
+        for(i = 0; i < P; i++){
+            int *ptr = hartals[i];
+            free(ptr);
+        }   
 	}	
 }
