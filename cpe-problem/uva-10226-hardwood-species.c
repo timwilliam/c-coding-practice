5f3fc1f5443afc02b0ad20fc5927a675ad772251
timwilliam <timothywilliam.cs06g@g2.nctu.edu.tw>
2021-07-12 11:21:45 +0800

    Completed uva/10226 (v2)

--- cpe-problem/uva-10226-hardwood-species.c
+++ cpe-problem/uva-10226-hardwood-species.c
@@ -1,102 +1,72 @@
-// Problem	: UVA 10226 - Hardwood Species
-// Author	: timwilliam
-// Compiled	: 05/12/2021
+// Problem  : UVA 10226 - Hardwood Species
+// Author   : timwilliam
+// Compiled : 07/12/2021
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void print_str(char **str, int n){
-	int i;
-	for(i = 0; i < n; i++){
-		printf("%s, ", str[i]);
-	}
-	printf("\n");
+int MAX_POP = 1000000;
+int MAX_SPECIES_NAME_LEN = 30;
+
+int cmpfunc(const void *a, const void*b){
+    return strcmp(*(char**) a, *(char**) b);
 }
 
-// sort the string ascendingly using insertion sort
-void sort_str(char **str, int n){
-	int key, i;
-	char word[30];
-	
-	for(i = 1; i < n; i++){
-		memset(word, 0, sizeof(word));
+void print_result(char **list_of_trees, int pop_count){
+    int i, species_count;
+    char current_species[30];
+    double percentage;
 
-		key = i;
-		strcpy(word, str[i]);
+    species_count = 1;
+    for(i = 0; i < pop_count; i++){
+        strcpy(current_species, list_of_trees[i]);
 
-		while(key > 0 && strcmp(word, str[key-1]) < 0){
-			strcpy(str[key], str[key-1]);
-			key--;	
-		}
+        if(strcmp(current_species, list_of_trees[i+1]) == 0 && i+1 < pop_count){
+            species_count++;
+        }
+        else{
+            percentage = (double) species_count / (double) pop_count * 100.0;
+            printf("%s %.4lf\n", current_species, percentage);
 
-		strcpy(str[key], word);
-	}
-}
+            species_count = 1;
+        }
+    }
 
-// count the population of the species
-void calculate_pop_percentage(char **str, int n_pop){
-	int i, current_species_count;
-	double percentage;
-	char current_species[30];
-
-	// other possible solution: using a combination of strstr() to get the first occurence of string,
-	// 					and using strchr() to get the last occurence of string
-
-	current_species_count = 1;
-	for(i = 0; i < n_pop; i++){
-		strcpy(current_species, str[i]);
-
-		if(strcmp(current_species, str[i+1]) == 0){
-			current_species_count++;
-		}
-		else{
-			percentage = (double) current_species_count / (double) n_pop * 100;			
-			printf("%s %.4lf\n", current_species, percentage);
-
-			current_species_count = 1;
-		}
-	}
+    return;
 }
 
 int main(void){
-	char **str_list, buffer[30];
-	int i, n_pop, POP_MAX, SPEC_MAX, SPEC_NAME_LEN, T;
+    int i, T, pop_count;
+    char **list_of_trees, input[32];
+    
+    list_of_trees = malloc(MAX_POP * sizeof(char*));
+    for(i = 0; i < MAX_POP; i++)
+        list_of_trees[i] = malloc(MAX_SPECIES_NAME_LEN * sizeof(char));
 
-	POP_MAX = 1000000;
-	SPEC_MAX = 10000;
-	SPEC_NAME_LEN = 30;
+    scanf("%d", &T);
+    getchar();
 
-	// allocate space to store the pointer to each string in the list
-	str_list = malloc(POP_MAX * sizeof(char*));
-	for(i = 0; i < POP_MAX; i++)
-		str_list[i] = malloc(SPEC_NAME_LEN * sizeof(char));
+    while(T--){
+        getchar();
 
-	scanf("%d", &T);
-	getchar();
+        pop_count = 0;
+        while(1){
+            fgets(input, 30, stdin);
+            if(input[0] == '\n')
+                break;
 
-	while(T--){
-		n_pop = -1;
-		while(1){
-			if(n_pop < 0){
-				getchar();
-				n_pop++;
-				continue;
-			}
+            input[strcspn(input, "\n")] = 0;
+            strcpy(list_of_trees[pop_count], input);
+            pop_count++;
+        }
 
-			memset(buffer, 0, sizeof(buffer));
-			fgets(buffer, 30, stdin);
+        qsort(list_of_trees, pop_count, sizeof(*list_of_trees), cmpfunc);
+        print_result(list_of_trees, pop_count);
+        
+        for(i = 0; i < MAX_POP; i++)
+            memset(list_of_trees[i], 0, MAX_SPECIES_NAME_LEN * sizeof(char));
+    }
 
-			if(buffer[0] == '\n')
-				break;
-
-			strcpy(str_list[n_pop], buffer);
-			str_list[n_pop][strcspn(str_list[n_pop], "\n")] = 0; // remove newline '\n'
-
-			n_pop++;
-		}
-
-		sort_str(str_list, n_pop); 
-		calculate_pop_percentage(str_list, n_pop);
-	}
+    return 0;
 }
