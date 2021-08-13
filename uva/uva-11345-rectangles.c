ee9e7e0ebeb87a5e6eea29c8dbb3c8bb16537802
timwilliam <19308063+timwilliam@users.noreply.github.com>
2021-08-13 17:09:52 +0800

    Initial commit of uva/11345

---
+++ uva/uva-11345-rectangles.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _RECT{
+    int x1, y1, x2, y2;
+}RECT;
+
+int getMax(int a, int b){
+    if(a >= b)
+        return a;
+    else
+        return b;
+}
+
+int getMin(int a, int b){
+    if(a < b)
+        return a;
+    else
+        return b;
+}
+
+int main(void){
+    int N, M, case_count, i, A;
+
+    case_count = 1;
+    scanf("%d", &N);
+
+    while(N--){
+        RECT temp, current;
+
+        // default value
+        temp.x1 = -10001;
+        temp.y1 = -10001;
+        temp.x2 = 10001;
+        temp.y2 = 10001;
+        A = 0;
+
+        scanf("%d", &M);
+        for(i = 0; i < M; i++){
+            scanf("%d %d %d %d", &current.x1, &current.y1, &current.x2, &current.y2);
+            temp.x1 = getMax(temp.x1, current.x1);
+            temp.y1 = getMax(temp.y1, current.y1);
+            temp.x2 = getMin(temp.x2, current.x2);
+            temp.y2 = getMin(temp.y2, current.y2);
+        }
+
+        if(temp.x1 < temp.x2 && temp.y1 < temp.y2)
+            A = (temp.x2 - temp.x1) * (temp.y2 - temp.y1);
+
+        printf("Case %d: %d\n", case_count, A);
+        case_count++;
+    }
+
+    return 0;
+}
